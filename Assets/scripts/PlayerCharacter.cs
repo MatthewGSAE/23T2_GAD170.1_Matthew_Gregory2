3f66fabbe58ee79a24311f75d07a5ff320b13e11
MatthewGSAE <136556271+MatthewGSAE@users.noreply.github.com>
2023-06-21 17:05:15 +1000

    i wanna die

--- Assets/scripts/PlayerCharacter.cs
+++ Assets/scripts/PlayerCharacter.cs
@@ -1,25 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Transactions;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = System.Random;
 
 public class PlayerCharacter : MonoBehaviour
 {
     // Variables will go first
-
+  public TMP_Text combatLog;
+    public TMP_Text statsText;
+   
+   
     [SerializeField] private double attack = 10;
     [SerializeField] private int health = 100;
     [SerializeField] private int level = 1;
     [SerializeField] private int experience = 0;
-    [SerializeField] private double exptolevelup = 10;
+    private double exptolevelup = 10;
+    private int enemyHealth;
 
-
-    public TMP_Text combatLog;
-
-    
-
-    // Methods will go underneath here
+    private bool isGameOver = false;
 
     // Start is called before the first frame update
     private void Start()
@@ -32,12 +36,21 @@ public class PlayerCharacter : MonoBehaviour
         Debug.Log("face my minions fool");
         combatLog.text = "Yo wassup";
 
+        enemyHealth = Random.Range(10, 15);
+
     }
 
     private void Update()
     {// attacking
-        if (Input.GetKeyDown(KeyCode.Space))
+      
+        
+       if (Input.GetKeyDown(KeyCode.Space))
         {
+           
+            if isPlayerCharacterTurn
+                (PlayerCharacter Attack)
+
+            
             Debug.Log("you have attacked the enemy");
             Debug.Log("attack: " + Mathf.Round((float)attack * 100) / 100);
         }
@@ -47,20 +60,41 @@ public class PlayerCharacter : MonoBehaviour
             Levelup();
         }
         // winning
-        if (level == 5)
-        {
-            combatLog.text = ("YOU HAVE BEATEN ME");
-            Debug.Log("YOU HAVE BEATEN ME");
-        }
+        
     }
 
+
+    private void attacking()
+    {
+
+    }
+  
+    
     // for leveling up
     private void Levelup()
     {
+      
+        
         attack *= 125.25;
         level++;
         exptolevelup *= 1.45;
         combatLog.text = "level up";
         Debug.Log("level up");
     }
+
+    private void Win()
+    {
+        if (level == 5)
+        {
+            combatLog.text = ("YOU HAVE BEATEN ME");
+            Debug.Log("YOU HAVE BEATEN ME");
+        }
+    }
 }
+
+
+    //sort out attacking
+  //if (isplayerturn)
+  //    PlayerAttack();
+ // else
+  //    EnemyAttack();
